796af5ffea0b18e33b1e73ec5e032fa45e7e2d6e
Osama Najjar <osama.najjar@mendix.com>
2019-01-02 13:56:12 +0100

    added cli questions

--- cli/questions.js
+++ cli/questions.js
@@ -4,5 +4,17 @@ module.exports = [
 		name: 'widgetName',
 		message: 'Widget Name:',
 		default: 'MyHyperMendixWidget'
+	},
+	{
+		type: 'input',
+		name: 'author',
+		message: 'Author',
+		default: 'John Doe'
+	},
+	{
+		type: 'input',
+		name: 'email',
+		message: 'Email:',
+		default: 'john.doe@example.com'
 	}
 ];
