f05479d0a3ff3ceb674510ebe7a2b28700f249bf
Osama Najjar <osama.najjar@mendix.com>
2019-01-05 23:40:30 +0100

    added new ins dirAlreadyExisited

--- cli/instructions.js
+++ cli/instructions.js
@@ -5,11 +5,14 @@ module.exports = {
 		console.log(
 			boxen(`${chalk.blue('CHMK - Create Hyper Mendix Widget')} ${chalk.bold('v.1.0.0')}`, {
 				padding: 1,
-				margin: 1,
+				margin: 2,
 				borderStyle: 'round',
 				borderColor: 'blue'
 			})
 		);
 		//console.log(boxen('Create Hyper Mendix Widget', { padding: 1 }));
+	},
+	dirAlreadyExisted(dirName) {
+		console.log(chalk.yellow(`There is already a dir called ${dirName}`));
 	}
 };
