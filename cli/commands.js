1fcf80b69f8e277ae6b38d828115501c71b383c6
Osama Najjar <osama.najjar@mendix.com>
2019-01-06 19:26:58 +0100

    next commands & related questions

--- cli/commands.js
+++ cli/commands.js
@@ -1,16 +1,114 @@
 const shell = require('shelljs');
-const fs = require('fs');
+const fs = require('fs-extra');
+const path = require('path');
 const chalk = require('chalk');
+const replace = require('replace');
 const { dirAlreadyExisted } = require('./instructions');
+const TEMPLATES = require('./TEMPLATES');
+const widgetCraetorModuleName = 'create-hyper-mendix-widget';
 
-module.exports = {
-	makeWidgetDir(dirName) {
-		const nextDirName = dirName.trim();
-		if (!fs.existsSync(nextDirName)) {
-			shell.mkdir(nextDirName);
-		} else {
-			dirAlreadyExisted(dirName);
-			process.exit(0);
-		}
+function _getWidgetCreatorModulePath() {
+	const npmGlobalModulesRoot = shell.exec('npm root -g', { silent: true }).stdout.trim();
+	const npmLocalModulesRoot = shell.exec('npm root', { silent: true }).stdout.trim();
+	const widgetCreatorGlobalModulePath = path.join(npmGlobalModulesRoot, widgetCraetorModuleName);
+	const widgetCreatorLocalModulePath = path.join(npmLocalModulesRoot, widgetCraetorModuleName);
+	// if the creator module was installed locally then favor it on the globally installed module.
+	if (fs.existsSync(widgetCreatorLocalModulePath)) {
+		return widgetCreatorLocalModulePath;
 	}
+	return widgetCreatorGlobalModulePath;
+}
+
+function _getTemplateName(userSelectedTemplate) {
+	if (userSelectedTemplate === 'Hyper!') return TEMPLATES.HYPER;
+	if (userSelectedTemplate === 'ES6 only!') return TEMPLATES.ES6;
+}
+
+function makeWidgetDir(dirName) {
+	if (!fs.existsSync(dirName)) {
+		return shell.mkdir(dirName).code === 0; // sucess
+	} else {
+		return false;
+	}
+}
+
+function copyWidgetFiles(dirName, selectedTemplate) {
+	const widgetCreatorModulePath = _getWidgetCreatorModulePath();
+	if (widgetCreatorModulePath) {
+		fs.copySync(
+			path.join(widgetCreatorModulePath, 'cli', 'templates', _getTemplateName(selectedTemplate)),
+			path.normalize(dirName)
+		);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function initWidget({ widgetName, description, author, email, initialVersion, license }) {
+	try {
+		replace({
+			regex: '<<widgetName>>',
+			replacement: widgetName,
+			paths: [ widgetName ],
+			recursive: true,
+			silent: true
+		});
+		replace({
+			regex: '<<widgetNamePcakge>>',
+			replacement: widgetName.toLowerCase(),
+			paths: [ widgetName ],
+			recursive: true,
+			silent: true
+		});
+		replace({
+			regex: '<<widgetDescription>>',
+			replacement: description,
+			paths: [ widgetName ],
+			recursive: true,
+			silent: true
+		});
+		replace({
+			regex: '<<version>>',
+			replacement: initialVersion,
+			paths: [ widgetName ],
+			recursive: true,
+			silent: true
+		});
+		replace({
+			regex: '<<authorName>>',
+			replacement: author,
+			paths: [ widgetName ],
+			recursive: true,
+			silent: true
+		});
+		replace({
+			regex: '<<authorEmail>>',
+			replacement: email,
+			paths: [ widgetName ],
+			recursive: true,
+			silent: true
+		});
+		replace({
+			regex: '<<license>>',
+			replacement: license,
+			paths: [ widgetName ],
+			recursive: true,
+			silent: true
+		});
+		return true;
+	} catch (error) {
+		return false;
+	}
+}
+
+function installDependencies(dirName) {
+	shell.cd(dirName);
+	return shell.exec('npm install', { silent: true }).code === 0; // success
+}
+module.exports = {
+	makeWidgetDir,
+	copyWidgetFiles,
+	initWidget,
+	installDependencies
 };
--- cli/questions.js
+++ cli/questions.js
@@ -3,18 +3,76 @@ module.exports = [
 		type: 'input',
 		name: 'widgetName',
 		message: 'Widget Name:',
-		default: 'MyHyperMendixWidget'
+		default: 'MyHyperMendixWidget',
+		validate: (input) => {
+			const validWidgetNameRegex = /^[0-9a-zA-Z_-]+$/;
+			const isValid = validWidgetNameRegex.test(input.trim());
+			!isValid && console.log('\nPlease enter a valid widget name!');
+			return isValid;
+		}
+	},
+	{
+		type: 'input',
+		name: 'description',
+		message: 'Widget description:',
+		default: 'My brand new hyper widget!',
+		validate: (input) => {
+			const isValid = input.trim().length > 0 && input.trim().length <= 500;
+			!isValid && console.log('\nPlease enter a valid widget description (no more than 500 characters)!');
+			return isValid;
+		}
 	},
 	{
 		type: 'input',
 		name: 'author',
 		message: 'Author',
-		default: 'John Doe'
+		default: 'John Doe',
+		validate: (input) => {
+			const isValid = input.trim().length > 0;
+			!isValid && console.log('\nPlease enter a valid author name!');
+			return isValid;
+		}
 	},
 	{
 		type: 'input',
 		name: 'email',
 		message: 'Email:',
-		default: 'john.doe@example.com'
+		default: 'john.doe@example.com',
+		validate: (input) => {
+			const validEmailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+			const isValid = validEmailRegex.test(input.trim());
+			!isValid && console.log('\nPlease enter a valid email address!');
+			return isValid;
+		}
+	},
+	{
+		type: 'input',
+		name: 'initialVersion',
+		message: 'Initial Version:',
+		default: '1.0.0',
+		validate: (input) => {
+			const validVersionRegex = /^(\d+\.)?(\d+\.)?(\*|\d+)$/;
+			const isValid = validVersionRegex.test(input.trim());
+			!isValid && console.log('\nPlease enter valid version number!');
+			return isValid;
+		}
+	},
+	{
+		type: 'input',
+		name: 'license',
+		message: 'License:',
+		default: 'MIT',
+		validate: (input) => {
+			const isValid = input.trim().length > 0;
+			!isValid && console.log('\nPlease enter a valid license!');
+			return isValid;
+		}
+	},
+	{
+		type: 'list',
+		name: 'template',
+		message: "Which template you'd like to use:",
+		choices: [ 'Hyper!', 'ES6 only!' ],
+		default: 'Hyper!'
 	}
 ];
