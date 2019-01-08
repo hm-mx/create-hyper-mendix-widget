e6a5a615d269b503ea50107d9c4097d26b698878
Osama Najjar <osama.najjar@mendix.com>
2019-01-08 01:17:52 +0100

    next es6 template

--- cli/templates/ES6/src/header.js
+++ cli/templates/ES6/src/header.js
@@ -1,5 +1,6 @@
-import { create as dojoCreate } from 'dojo/construct';
-
-export default function(DummyText) {
-    return dojoCreate('h1', { class: 'header' });
+import { create as dojoCreate, place as dojoPlace } from 'dojo/dom-construct';
+export default function(dummyText) {
+    const headerWrapper = dojoCreate('div', { class: 'custom-header' });
+    dojoPlace(dojoCreate('h1', { innerHTML: dummyText }), headerWrapper);
+    return headerWrapper;
 }
