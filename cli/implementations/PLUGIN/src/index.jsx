0d4d27814f9dc974ad15ba5586ead0a789d2f039
Osama Najjar <osama.najjar@mendix.com>
2019-01-25 11:38:28 +0100

    set main and preview src

--- cli/implementations/PLUGIN/src/index.jsx
+++ cli/implementations/PLUGIN/src/index.jsx
@@ -1,20 +1,6 @@
-import _widgetBase from 'MxWidgetBase';
-import declare from 'dojoBaseDeclare';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import * as widgetConf from '../conf/widget.config.json';
 import './style/style.scss';
 
 import Counter from './Counter';
 
-export default declare(`${widgetConf.name}.widget.${widgetConf.name}`, [ _widgetBase ], {
-    constructor() {},
-    postCreate() {
-        console.debug(`${this.id} >> postCreate`);
-    },
-    update(contextObject, callback) {
-        console.debug(`${this.id} >> update`);
-        ReactDOM.render(<Counter dummyKey={this.dummyKey}/>, this.domNode);
-        callback();
-    }
-});
+export default (props) => <Counter {...props} />;
---
+++ cli/implementations/PLUGIN/src/index.preview.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style/style.scss';
+import Counter from './Counter';
+
+export class Preview extends React.Component {
+    render() {
+        return <Counter />;
+    }
+}
