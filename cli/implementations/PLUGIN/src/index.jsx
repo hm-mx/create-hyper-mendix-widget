d18dca779c82bde4fb5af297ff1cf79f19f7efcd
Osama Najjar <osama.najjar@mendix.com>
2019-02-05 14:46:39 +0100

    deleted in-progress implementation

--- cli/implementations/PLUGIN/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './style/style.scss';
-
-import Counter from './Counter';
-
-export default (props) => <Counter {...props} />;
--- cli/implementations/PLUGIN/src/index.preview.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './style/style.scss';
-import Counter from './Counter';
-
-export class Preview extends React.Component {
-    render() {
-        return <Counter />;
-    }
-}
