60effdbf9ae7cafb54c1e054065e730ffb3d293b
Huang-Ming Chang <huang.ming.chang@mendix.com>
2019-07-31 10:51:14 +0200

    new webpack settings that allows preview

---
+++ cli/implementations/REACT_MX7/generateMpkFile.js
@@ -0,0 +1,33 @@
+const zip = require('bestzip'); // eslint-disable-line import/no-unresolved
+const path = require('path');
+const fs = require('fs-extra');
+
+const { widgetName } = require('./package.json');
+const paths = require('./paths');
+
+const zipToFolders = async () => {
+  try {
+    await zip({
+      source: '.',
+      destination: `./${widgetName}.mpk`,
+      cwd: path.join(process.cwd(), 'build'),
+    });
+
+    console.log(`Generated ${widgetName}.mpk`); // eslint-disable-line no-console
+
+    const { mxProjectRootDir } = paths;
+    if (mxProjectRootDir) {
+      await fs.copy(
+        `./build/${widgetName}.mpk`,
+        `${mxProjectRootDir}/widgets/${widgetName}.mpk`
+      );
+
+      console.log(`Copied to your Mendix project: ${mxProjectRootDir}`); // eslint-disable-line no-console
+    }
+  } catch (err) {
+    console.error(err); // eslint-disable-line no-console
+    process.exit(1);
+  }
+};
+
+zipToFolders();
---
+++ cli/implementations/REACT_MX7/src/index.webmodeler.js
@@ -0,0 +1,27 @@
+import React from 'react'; // eslint-disable-line import/no-unresolved
+
+import Counter from './components/Counter';
+import styleAsString from './style/style.scss';
+
+/**
+ * This file is to define how your widget is shown in
+ *  - Studio Pro: Design Mode (Mendix 8) / View Mode (Mendix 7)
+ *  - Studio
+ *
+ * You need to export `preview` as a normal React component.
+ * NOTE: it has to be lower-case, named export.
+ *
+ * It's better that `preview` is only a dumb component,
+ * because the whole purpose of this file is to show "HOW IT LOOKS".
+ *
+ * To inject css for your `preview`, export another named function `getPreviewCss`
+ * This function should return compiled css as string.
+ * We use `@researchgate/babel-plugin-transform-scss-import-to-string`
+ * so we can just import our scss as string.
+ */
+
+export const preview = () => <Counter />;
+
+export function getPreviewCss() {
+  return styleAsString;
+}
