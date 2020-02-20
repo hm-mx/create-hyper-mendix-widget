e16e816086f7728b878978a5c55cc3aef8784e11
Huang-Ming Chang <huang.m.chang@gmail.com>
2020-02-20 09:45:42 +0100

    Update DOJO examples

--- cli/implementations/DOJO_TS/src/components/Counter.tsx
+++ cli/implementations/DOJO_TS/src/components/Counter.tsx
@@ -1,60 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentProps } from 'typings/BaseProps';
 
-interface CounterState {
-  count: number;
-}
+const Counter = ({ style, dummyKey }: ComponentProps) => {
+  const [count, setCount] = useState<number>(0);
+  const down = setCount(count - 1);
+  const up = setCount(count + 1);
 
-class Counter extends React.Component<ComponentProps, CounterState> {
-  state = { count: 0 };
-
-  /**
-   * in case your widget requires context
-   * i.e. needsEntityContext="true" in `widget.config.xml`
-   */
-  componentDidMount() {
-    const { mxObject } = this.props;
-    if (mxObject) {
-      // Do something one time when component is rendered
-    }
-  }
-
-  componentDidUpdate() {
-    const { mxObject } = this.props;
-    if (mxObject) {
-      // Do something that will fire every time when context is changed
-    }
-  }
-
-  down = () => {
-    this.setState(({ count }) => ({ count: count - 1 }));
-  };
-
-  up = () => {
-    this.setState(({ count }) => ({ count: count + 1 }));
-  };
-
-  render() {
-    const { count } = this.state;
-    const { style, dummyKey } = this.props;
-
-    return (
-      <div className="react-counter" style={style}>
-        <div className="counter-header">
-          <h1>{dummyKey}</h1>
-        </div>
-        <h1 className="counter-count">{count}</h1>
-        <div className="controls-wrapper">
-          <button type="button" className="counter-btn" onClick={this.down}>
-            -
-          </button>
-          <button type="button" className="counter-btn" onClick={this.up}>
-            +
-          </button>
-        </div>
+  return (
+    <div className="react-counter" style={style}>
+      <div className="counter-header">
+        <h1>{dummyKey}</h1>
       </div>
-    );
-  }
-}
+      <h1 className="counter-count">{count}</h1>
+      <div className="controls-wrapper">
+        <button type="button" className="counter-btn" onClick={down}>
+          -
+        </button>
+        <button type="button" className="counter-btn" onClick={up}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Counter;
